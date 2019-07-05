c616373d185b64168af4a05a03756f07921aaffa
Aleksandr Golubovskij <aleksandr@hexito.com>
2019-07-05 14:01:30 +0300

    statuses

--- src/Kyc/PossibleStatuses.php
+++ src/Kyc/PossibleStatuses.php
@@ -5,8 +5,13 @@ namespace Velser\OndatoApiClient\Kyc;
 
 final class PossibleStatuses
 {
+    const STATUS_BAD_REQUEST = 'IDENTIFICATION_NOT_FOUND';
+    const STATUS_IDENTIFICATION_NOT_FOUND = 'IDENTIFICATION_NOT_FOUND';
+    const STATUS_SESSION_NOT_CREATED = 'SESSION_NOT_CREATED';
+
     const STATUS_APPROVED = 'APPROVED';
     const STATUS_SUCCESSFUL = 'SUCCESSFUL';
     const STATUS_PENDING = 'PENDING';
+    
     const STATUS_FAILED = 'FAILED';
 }
